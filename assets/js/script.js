f18168ccfd884d9d84c1fe9228d5a91257692b5e
Shruti Gupta <113260340+shrutigupta263@users.noreply.github.com>
2025-01-27 07:05:34 +0000

    chng

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,17 @@ const addEventOnElements= function (elements, eventType, callback)
 const preloader = document.querySelector("[data-preloader]");
 window.addEventListener("load", function () {
     preloader.classList.add("loaded");
-    this.document.body.classList.add("loaded");
+    document.body.classList.add("loaded");
 });
 /** mobile nav toggle */
 const navbar = document.querySelector("[data-navbar]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 const toggleNavbar = function () { navbar.classList.toggle("active");}
-navToggler.addEventListener("click", toggleNavabr);
+navToggler.addEventListener("click", toggleNavbar);
 /** HEADER */
 const header = document.querySelector("[data-header]");
 const activeHeader =  function() {
-     window.scrollY > 50 ? Headers.classList.add("active")
+     window.scrollY > 50 ? header.classList.add("active")
      : header.classList.remove("active");
 }
 window.addEventListener("scroll",activeHeader);
